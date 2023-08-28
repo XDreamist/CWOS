f539435e288d372071045e46c9619c240cf7a02a
XD <rockistangos@gmail.com>
2023-08-28 13:45:06 +0530

    Host

--- chipworld/models.py
+++ chipworld/models.py
@@ -25,6 +25,7 @@ class Product(models.Model):
     image4 = models.ImageField(upload_to='product_images/', blank=True)
     price = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
+    specification = models.TextField(max_length=500)
     new_arrival = models.BooleanField(default=False)
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
 
@@ -34,36 +35,8 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Printer(Product):
-    print_type = models.CharField(max_length=50)
-    speed = models.CharField(max_length=50)
-    resolution = models.CharField(max_length=50)
-    connectivity = models.CharField(max_length=50)
-    paper_handling = models.CharField(max_length=50)
-
-    def save(self, *args, **kwargs):
-        if not self.type:
-            self.type = "Printer"
-        super(Printer, self).save(*args, **kwargs)
-
-class Computer(Product):
-    processor = models.CharField(max_length=50)
-    ram = models.CharField(max_length=50)
-    memory = models.CharField(max_length=50)
-    gpu = models.CharField(max_length=50)
-
-    def save(self, *args, **kwargs):
-        if not self.type:
-            self.type = "Computer"
-        super(Computer, self).save(*args, **kwargs)
-
-class Cctv(Product):
-    resolution = models.CharField(max_length=50)
-    nightvision = models.CharField(max_length=50)
-    fov = models.CharField(max_length=50)
-    connectivity = models.CharField(max_length=50)
-
-    def save(self, *args, **kwargs):
-        if not self.type:
-            self.type = "Cctv"
-        super(Cctv, self).save(*args, **kwargs)
+class Slides(models.Model):
+    heading = models.CharField(max_length=50)
+    content = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='slide_images/', blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- chipworld/views.py
+++ chipworld/views.py
@@ -8,6 +8,7 @@ def homepage(request):
     products = Product.objects.all()
     product_type = ProductType.objects.all()
     new_prod = Product.objects.filter(new_arrival = True)
+
     return render(request, 'index.html', {'products': products, 'product_type': product_type, 'new_prod': new_prod})
 
 def shoppage(request):
@@ -22,12 +23,5 @@ def contactpage(request):
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     product_type = ProductType.objects.all()
-    prod_details = None
 
-    if product.type == "Computer":
-        prod_details = Computer.objects.filter(name=product.name).first()
-        print("Computer details:", prod_details)
-    elif product.type == "Cctv":  prod_details = Cctv.objects.filter(name=product.name).first()
-    elif product.type == "Printer":  prod_details = Printer.objects.filter(name=product.name).first()
-
-    return render(request, 'shop-single.html', {'product': product, 'product_type': product_type, 'details': prod_details})
+    return render(request, 'shop-single.html', {'product': product, 'product_type': product_type})
